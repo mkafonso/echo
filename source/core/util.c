0aec6a2c84ecb42e7729a6c663f15c49a61f02b2
mkafonso <mkafonso.dev@gmail.com>
2026-04-10 23:18:03 -0300

    feat: add file I/O and secure zero utility functions

---
+++ source/core/util.c
@@ -0,0 +1,88 @@
+#include "echo/util.h"
+#include "echo/errors.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+echo_error_t echo_read_file(
+    const char *path,
+    uint8_t **out_data,
+    size_t *out_len
+) {
+    FILE *fp = NULL;
+    uint8_t *buffer = NULL;
+    long size = 0;
+
+    if (!path || !out_data || !out_len) {
+        return ECHO_ERR_INVALID_ARG;
+    }
+
+    fp = fopen(path, "rb");
+    if (!fp) {
+        return ECHO_ERR_IO;
+    }
+
+    if (fseek(fp, 0, SEEK_END) != 0) {
+        fclose(fp);
+        return ECHO_ERR_IO;
+    }
+
+    size = ftell(fp);
+    if (size < 0) {
+        fclose(fp);
+        return ECHO_ERR_IO;
+    }
+
+    rewind(fp);
+
+    buffer = (uint8_t *)malloc((size_t)size);
+    if (!buffer) {
+        fclose(fp);
+        return ECHO_ERR_NOMEM;
+    }
+
+    if (size > 0 && fread(buffer, 1, (size_t)size, fp) != (size_t)size) {
+        free(buffer);
+        fclose(fp);
+        return ECHO_ERR_IO;
+    }
+
+    fclose(fp);
+
+    *out_data = buffer;
+    *out_len = (size_t)size;
+    return ECHO_OK;
+}
+
+echo_error_t echo_write_file(
+    const char *path,
+    const uint8_t *data,
+    size_t len
+) {
+    FILE *fp = NULL;
+
+    if (!path || (!data && len > 0)) {
+        return ECHO_ERR_INVALID_ARG;
+    }
+
+    fp = fopen(path, "wb");
+    if (!fp) {
+        return ECHO_ERR_IO;
+    }
+
+    if (len > 0 && fwrite(data, 1, len, fp) != len) {
+        fclose(fp);
+        return ECHO_ERR_IO;
+    }
+
+    fclose(fp);
+    return ECHO_OK;
+}
+
+void echo_secure_zero(void *ptr, size_t len) {
+    volatile unsigned char *p = (volatile unsigned char *)ptr;
+    while (len--) {
+        *p++ = 0;
+    }
+}
